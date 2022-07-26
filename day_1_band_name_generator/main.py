8dcc1dd5de537cb913c5f67e81696df33d7b01af
Naman Thanki <namanthanki@users.noreply.github.com>
2022-07-26 22:54:21 +0530

    first day challenge

---
+++ day_1_band_name_generator/main.py
@@ -0,0 +1,10 @@
+#Day-1-Challenge: Band Name Generator
+
+print("Welcome to Band Name Generator!!!\n")
+
+user_city = input("What city did you grow up in?\n")
+user_pet = input("\nWhat is your pet's name?\n")
+
+band_name = user_city + ' ' + user_pet
+
+print("\nYour Band Name is: " + band_name)
