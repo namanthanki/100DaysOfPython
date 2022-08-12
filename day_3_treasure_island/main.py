b0d31fc733d943e6c339c4be52308ebb6a522a36
Naman Thanki <namanthanki@users.noreply.github.com>
2022-08-12 19:21:23 +0530

    refactored

--- day_3_treasure_island/main.py
+++ day_3_treasure_island/main.py
@@ -1,23 +1,48 @@
-print("Welcome to the Treasure Island\n")
-print("Your Mission is to Find Treasure\n")
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
 game_over = False
 
 while game_over == False:
-    path = input("left or right")
+    path = input("You're at a cross road. Where do you want to go? Type 'left' or 'right' \n").lower()
     if(path == "right"):
+        print("You fell into a hole. Game Over.")
         game_over = True
         break
     else: 
-        swim_or_wait = input("swim or wait")
+        swim_or_wait = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. \n").lower()
         if(swim_or_wait == "swim"):
+            print("You get attacked by an angry trout. Game Over.")
             game_over = True
             break
         else:
-            door = input("Which door (Red, Blue or Yellow): ")
+            door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
             if(door == "red" or door == "blue"):
+                print("You got Trapped, Game Over.")
                 game_over = True
                 break
             else: 
-                print("You win")
+                print("You Found the Treasure! You Win!")
                 break
