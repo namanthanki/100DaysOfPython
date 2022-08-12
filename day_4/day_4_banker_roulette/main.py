3244957fcc741c34652fc2f4d2e65add07e21736
Naman Thanki <namanthanki@users.noreply.github.com>
2022-08-13 00:24:47 +0530

    day_4

--- day_4/day_4_banker_roulette/main.py
+++ day_4/day_4_banker_roulette/main.py
@@ -6,8 +6,17 @@ print("Alight then! it's time to pay the bill now\n")
 names = input("Everyone!, give me your names (seperated by commas): ")
 
 names = names.replace(" ", "")
+
+if not names:
+    print("Invalid Input")
+    exit()
+
 names = names.split(",")
 
-bill_payer = names[random.randint(0, len(names) - 1)]
+length = len(names)
 
+if length == 1:
+    bill_payer = names[0]
+else:
+    bill_payer = names[random.randint(0, length - 1)]
 print(f"{bill_payer} will pay this time for all the Delicious Food!")
