051323d118798b56f185e9c321734d3bd9aeecbf
Naman Thanki <namanthanki@users.noreply.github.com>
2022-08-13 18:46:58 +0530

    day_6

---
+++ day_6/main.py
@@ -0,0 +1,9 @@
+#Intro to Functions
+#Declared using 'def' keyword
+#has number of arguments seperated by comma (,) inside ()
+#such arguments are copies of values passed down at time of calling a function
+
+def foo(foo_bar): 
+    print(foo_bar)
+
+foo("foo")
