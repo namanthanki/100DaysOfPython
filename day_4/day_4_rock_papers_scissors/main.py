21c8b79ad765ab1abaa70101ab3c74dd135f8257
Naman Thanki <namanthanki@users.noreply.github.com>
2022-08-13 14:19:35 +0530

    added_ascii_art

--- day_4/day_4_rock_papers_scissors/main.py
+++ day_4/day_4_rock_papers_scissors/main.py
@@ -1,18 +1,49 @@
 import random
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
 print("Rock: \t  (1)\nPaper: \t  (2)\nScissors: (3)\n")
 user_choice = int(input("Your call?: "))
 choices = [ "Rock", "Paper", "Scissors" ]
 choice_lookup = [ 1, 2, 3 ]
+game_art = [ rock, paper, scissors ]
 
 if user_choice not in choice_lookup: 
     print("Invalid Input, Please try values from 1, 2 or 3")
     exit()
 else: 
     print(f"You chose     : {choices[user_choice - 1]}")
+    print(game_art[user_choice - 1])
 
-computer_choice = choices[random.randint(0, 2)]
+rand_index = random.randint(0, 2)
+computer_choice = choices[rand_index]
 print(f"Computer chose: {computer_choice}")
+print(game_art[rand_index])
 
 if choices[user_choice - 1] == computer_choice:
     print("It's a Draw!")
